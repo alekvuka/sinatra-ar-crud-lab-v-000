e8cb5a4c6bfb961a8783466e6b2661b2c44ed112
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-12-10 19:50:10 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,19 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+  attr_accessor :title, :content
+  @@all = Array.new
+
+  def initialize(params)
+    binding.pry
+    @title = params[:title]
+    @content = params[:content]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
 end
